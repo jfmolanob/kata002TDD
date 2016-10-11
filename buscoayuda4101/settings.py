0194d96ef9a8d3e991a91eec8eb1e4f348dc245f
José Miguel <jm.suarez201@uniandes.edu.co>
2016-10-11 14:37:08 -0500

    rojo-paso1

--- buscoayuda4101/settings.py
+++ buscoayuda4101/settings.py
@@ -136,12 +136,12 @@ ALLOWED_HOSTS = ['*']
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-#STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-#STATICFILES_DIRS = [
-    #os.path.join(PROJECT_ROOT, 'static'),
-#]
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_ROOT, 'static'),
+]
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
